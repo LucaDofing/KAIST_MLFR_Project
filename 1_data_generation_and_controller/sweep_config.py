7de5ee2e790b3f5f9341353990abc381487988f6
Michael Piltz <ge93cik@mytum.de>
2025-06-04 10:56:44 +0900

    sweep constant damping

--- 1_data_generation_and_controller/sweep_config.py
+++ 1_data_generation_and_controller/sweep_config.py
@@ -1,15 +1,15 @@
 xml_params = {
     "n_links": [1],
-    "link_length": [0.15],
-    "link_radius": [0.01],
-    "link_density": [500,1000],
-    "joint_damping": [0,0.2,0.4,0.6,0.8,1.0],
+    "link_length": [0.15,0.3],
+    "link_radius": [0.01,0.05],
+    "link_density": [500,1000,1500],
+    "joint_damping": [0.4],
     "joint_friction": [0.0],
 }
 
 sim_params = {
     "control_mode": ["pd"],
-    "initial_angle": [90.0],
+    "initial_angle": [90.0,180,30],
     "sim_time": [4.0],
     "target_angle": [45.0],
     "kp": [0.5,3.0],
